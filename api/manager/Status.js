b97b20c6a3af5c2200e1fb91577adee6593c8e4c
Saumava Dutta <152066316+SaumavaDutta@users.noreply.github.com>
2023-12-02 00:51:41 +0530

    Update Status.js

--- api/manager/Status.js
+++ api/manager/Status.js
@@ -12,6 +12,6 @@ module.exports = {
 
 /* Made
 *  By
-*  Discord Id - ashton.gg (Basic Implementation of Discord.jsV13 Library)
+*  Discord Id - Saumava (Basic Implementation of Discord.jsV13 Library)
 *  Credits must be there
 */
