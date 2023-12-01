d4fedf172b2fc5d7d9c2cfbcd2165e35663f65ca
Saumava Dutta <152066316+SaumavaDutta@users.noreply.github.com>
2023-12-02 00:49:15 +0530

    Update Message.js

--- api/components/Message.js
+++ api/components/Message.js
@@ -571,6 +571,6 @@ module.exports.deletedMessages = deletedMessages;
 
 /* Made
 *  By
-*  Discord Id - ashton.gg (Basic Implementation of Discord.jsV13 Library)
+*  Discord Id - Saumava (Basic Implementation of Discord.jsV13 Library)
 *  Credits must be there
 */
