b10a28f67e52b7d3d728506616119c895f2ca058
Saumava Dutta <152066316+SaumavaDutta@users.noreply.github.com>
2023-12-02 00:50:49 +0530

    Update IntentsBitFieldManager.js

--- api/manager/IntentsBitFieldManager.js
+++ api/manager/IntentsBitFieldManager.js
@@ -10,6 +10,6 @@ module.exports = IntentsBitField;
 
 /* Made
 *  By
-*  Discord Id - ashton.gg (Basic Implementation of Discord.jsV13 Library)
+*  Discord Id - Saumava (Basic Implementation of Discord.jsV13 Library)
 *  Credits must be there
 */
