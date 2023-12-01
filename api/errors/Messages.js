a108c55c6e2d536280f7703d35f9b9a7937559b2
Saumava Dutta <152066316+SaumavaDutta@users.noreply.github.com>
2023-12-02 00:49:55 +0530

    Update Messages.js

--- api/errors/Messages.js
+++ api/errors/Messages.js
@@ -174,7 +174,7 @@ for (const [name, message] of Object.entries(Messages)) register(name, message);
 
 /* Made
 *  By
-*  Discord Id - ashton.gg (Basic Implementation of Discord.jsV13 Library)
+*  Discord Id - Saumava (Basic Implementation of Discord.jsV13 Library)
 *  Credits must be there
 */
 
