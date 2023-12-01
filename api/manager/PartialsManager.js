7f1162e6887f26ac4ad5bfa3f63e2ae267b33567
Saumava Dutta <152066316+SaumavaDutta@users.noreply.github.com>
2023-12-02 00:51:06 +0530

    Update PartialsManager.js

--- api/manager/PartialsManager.js
+++ api/manager/PartialsManager.js
@@ -11,6 +11,6 @@ module.exports = createEnum([
 
 /* Made
 *  By
-*  Discord Id - ashton.gg (Basic Implementation of Discord.jsV13 Library)
+*  Discord Id - Saumava (Basic Implementation of Discord.jsV13 Library)
 *  Credits must be there
 */
